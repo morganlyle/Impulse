61372e0d5b0d989b2413dc7ab9e23c79bc72c8da
Morgan Lyle <morganlylee@gmail.com>
2022-07-11 17:52:35 -0400

    pre-pill

--- subscriptions/subscriptions_rest/models.py
+++ subscriptions/subscriptions_rest/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 # Create your models here.
-# class Subscriptions(models.Model):
-#     name = models.CharField(max_length=255, blank=True)
+class Subscriptions(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(models.ForeignKey)
+    items = models.ManyToManyField(models.ForeignKey)
+    
+    
+class Items(models.Model):
+    pass
+
+class Users(models.Model):
+    pass
