98c832afd80f1a46bd525ef3e3a84b910a42d264
rbbitphoto <rbbitphoto@gmail.com>
2022-07-11 17:11:20 -0700

    commented sub models otu to get it running, modified manage.py for products

--- subscriptions/subscriptions_rest/models.py
+++ subscriptions/subscriptions_rest/models.py
@@ -1,14 +1,14 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
-class Subscriptions(models.Model):
-    name = models.CharField(max_length=255, blank=True)
-    user = models.ForeignKey(models.ForeignKey)
-    items = models.ManyToManyField(models.ForeignKey)
+# # Create your models here.
+# class Subscriptions(models.Model):
+#     name = models.CharField(max_length=255, blank=True)
+#     user = models.ForeignKey(models.ForeignKey)
+#     items = models.ManyToManyField(models.ForeignKey)
     
     
-class Items(models.Model):
-    pass
+# class Items(models.Model):
+#     pass
 
-class Users(models.Model):
-    pass
+# class Users(models.Model):
+#     pass
