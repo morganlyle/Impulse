85507f8f774af9343a37d387273f7e6bbdb4dbec
Morgan Lyle <morganlylee@gmail.com>
2022-07-12 19:36:21 -0400

    no change but making me commit

--- subscriptions/subscriptions_rest/models.py
+++ subscriptions/subscriptions_rest/models.py
@@ -1,14 +1,20 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Subscriptions(models.Model):
-#     name = models.CharField(max_length=255, blank=True)
-#     user = models.ForeignKey(models.ForeignKey)
-#     items = models.ManyToManyField(models.ForeignKey)
-    
-    
-# class Items(models.Model):
-#     pass
+class Subscriptions(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(UserVO, related_name='user', on_delete=models.PROTECT)
+       
+    def __str__(self):
+        return self.name
 
-# class Users(models.Model):
-#     pass
+class ModelNumber(models.Model):
+    pass
+class ProductsBox(models.Model):
+    item_name = models.CharField(max_length=255, blank=True)
+    
+    pass
+
+class ClothingBox(models.Model):
+    pass
+class UserVO(models.Model):
+    pass
