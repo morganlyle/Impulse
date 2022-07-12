f1c764010b1f550e46c3b0fe1ea1d57f831603b9
Evan <evanx150@gmail.com>
2022-07-11 19:00:55 -0500

    nuked and re-made products

--- products/api/products_rest/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# from django.urls import reverse
-
-# Create your models here.
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-
