768453bfc76dd81e1f2dc34465fa9b1c9f32363c
Nellie <nellienelson@protonmail.com>
2022-07-12 16:47:32 -0700

    Deleted clothing as a separate microservice and merged it into Products.

--- products/api/products_rest/models.py
+++ products/api/products_rest/models.py
@@ -7,3 +7,23 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
+
+class Blouses(models.Model):
+    name =
+    color =
+    size =
+
+    unit_id = # a unique SKU
+    pass
+
+
+class Kilts(models.Model):
+    name =
+    color =
+    size =
+
+    pass
+
+
+class Accessories(models.Model):
+    pass
