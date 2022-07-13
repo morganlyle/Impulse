4395c827424cc6700bf565b4b301d94ddee8aad3
Morgan Lyle <morganlylee@gmail.com>
2022-07-13 19:50:52 -0400

    views and models done

--- subscriptions/api/subscriptions_rest/models.py
+++ subscriptions/api/subscriptions_rest/models.py
@@ -7,21 +7,20 @@ class ProductInventoryVO(models.Model):
 class ClothingInventoryVO(models.Model):
     sku = models.PositiveSmallIntegerField(max_length=50, blank = True)
     product = models.CharField(max_length=200, blank = True)
-    
 class ClothingBox(models.Model):
     name = models.CharField(max_length=100, blank = True)
     clothing_items = models.ManyToManyField(ClothingInventoryVO, related_name="clothing")
-
 class ProductsBox(models.Model):
     name = models.CharField(max_length=100, blank = True)
     product_items = models.ManyToManyField(ProductInventoryVO, related_name="products")
-
 class User(models.Model):
     username = models.CharField(max_length=50, unique= True)
+    password = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique = True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     subscriptions = models.BooleanField(default=False)
+    address = models.CharField(max_length=200)
 class Subscription(models.Model):
     username = models.ForeignKey(User, related_name="subscription", on_delete=models.PROTECT, unique=True)
     model_number = models.PositiveSmallIntegerField(max_length=20, unique = True)
