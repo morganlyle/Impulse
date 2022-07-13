0155cb45b02b749941e20efd15336d223709d234
rbbitphoto <rbbitphoto@gmail.com>
2022-07-13 14:25:12 -0700

    moved subscriptions into api folder and added poller

--- products/products_rest/models.py
+++ products/products_rest/models.py
@@ -3,27 +3,27 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+# class Product(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.CharField(max_length=200)
 
 
-class Blouses(models.Model):
-    name =
-    color =
-    size =
+# class Blouses(models.Model):
+#     name =
+#     color =
+#     size =
 
-    unit_id = # a unique SKU
-    pass
-
-
-class Kilts(models.Model):
-    name =
-    color =
-    size =
-
-    pass
+#     unit_id = # a unique SKU
+#     pass
 
 
-class Accessories(models.Model):
-    pass
+# class Kilts(models.Model):
+#     name =
+#     color =
+#     size =
+
+#     pass
+
+
+# class Accessories(models.Model):
+#     pass
