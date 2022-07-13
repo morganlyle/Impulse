2c79c611243b5c6181c2f4fa15180ef57c647897
rbbitphoto <rbbitphoto@gmail.com>
2022-07-13 15:17:29 -0700

    created encoders and tidied up models

--- subscriptions/api/subscriptions_rest/models.py
+++ subscriptions/api/subscriptions_rest/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class ProductInventoryVO(models.Model):
     sku = models.PositiveSmallIntegerField(max_length=50, blank = True)
     product = models.CharField(max_length=200, blank = True)
+
 class ClothingInventoryVO(models.Model):
     sku = models.PositiveSmallIntegerField(max_length=50, blank = True)
     product = models.CharField(max_length=200, blank = True)
-    
+
 class ClothingBox(models.Model):
     name = models.CharField(max_length=100, blank = True)
     clothing_items = models.ManyToManyField(ClothingInventoryVO, related_name="clothing")
@@ -18,16 +19,20 @@ class ProductsBox(models.Model):
 
 class User(models.Model):
     username = models.CharField(max_length=50, unique= True)
+    password = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique = True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     subscriptions = models.BooleanField(default=False)
+    address = models.CharField(max_length=200)
+
 class Subscription(models.Model):
     username = models.ForeignKey(User, related_name="subscription", on_delete=models.PROTECT, unique=True)
     model_number = models.PositiveSmallIntegerField(max_length=20, unique = True)
     price = models.PositiveSmallIntegerField(default=30)
     products = models.ForeignKey(ProductsBox, related_name="subscription", on_delete=models.PROTECT, null=True, blank=True)
     clothing = models.ForeignKey(ClothingBox, related_name='subscription', on_delete=models.PROTECT, null=True, blank=True)
+
 class Receipt(models.Model):
     username = models.ForeignKey(User, related_name="receipt",
         on_delete=models.CASCADE)
