282f3d8de061a586ea858e16b91ab1ed64d47600
Evan <evanx150@gmail.com>
2022-07-13 17:15:18 -0500

    some stuff added

--- products/encoders/encoders.py
+++ products/encoders/encoders.py
@@ -1,1 +1,28 @@
-from json import JSONEncoder
+from encoders.json import ModelEncoder
+
+from products_rest.models import Product_Category, Product, Clothing
+
+class ProductCategoryEncoder(ModelEncoder):
+    model = Product_Category
+    properties = [
+        "category",
+    ]
+
+class ProductEncoder(ModelEncoder):
+    model = Product
+    properties = [
+        "name",
+        "description",
+        "picture_url",
+        "product_category",
+    ]
+
+class ClothingEncoder(ModelEncoder):
+    model = Clothing
+    properties = [
+        "name",
+        "color",
+        "size",
+        "product_category",
+    ]
+
--- products/products_rest/views.py
+++ products/products_rest/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_http_methods
 import json
 
 from .models import Product_Category, Product, Clothing
+from encoders.encoders import ProductCategoryEncoder, ProductEncoder, ClothingEncoder
 
 # Create your views here.
 
@@ -13,5 +14,20 @@ def api_product_category(request):
         product_categories =  Product_Category.objects.all()
         return JsonResponse(
             {"product_categories": product_categories},
-            
+            encoder=ProductCategoryEncoder,
         )
+    else:
+        try:
+            content = json.loads(request.body)
+            category = content["category"]
+            return JsonResponse(
+                product_categories,
+                encoder=ProductCategoryEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not create the product category"}
+            )
+            response.status_code = 400
+            return response
