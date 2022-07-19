a9038b5c6ae8d6f2d8419377fdb9d225d2c572df
Morgan Lyle <morganlylee@gmail.com>
2022-07-19 19:16:39 -0400

    added some mdbootstrap, finished footer

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,32 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomePage from './Homepage.js';
 import Footer from './Footer.js';
 import './App.css';
-import Nav from './Nav.js';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
-// import Nav from './Nav';
 import SignUpForm from './SignUpForm';
 import './index.css';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Nav />
+      <NavBar />
       <div className="container">
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='footer' element={<Footer />} />
+
 
         </Routes>
 
-        <NavBar />
 
         <Routes>
           {/* <Route path='/' element={<Homepage />} /> */}
           <Route path='signup/' element={<SignUpForm />} />
         </Routes>
       </div>
+      <Footer />
     </BrowserRouter>
 
   );
--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,45 +1,124 @@
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBIcon, MDBInput,
+    MDBCol, MDBRow,
+    MDBBtn, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem, MDBDropdownLink
+} from 'mdb-react-ui-kit';
 
-function Footer() {
+export default function Footer() {
     return (
-        <div href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom ">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-3 border-top">
-                <li className="nav-item">
-                    <NavLink className="link-dark text-decoration-none" to="/">
-                        Home
-                    </NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="link-dark text-decoration-none" to="/">
-                        About Us
-                    </NavLink>
-                </li>
-                <li className="nav-link px-2 text-muted">Connect: </li>
-                <ul>
-                    <li>
-                        <a href="https://www.linkedin.com/"
-                            className="dropdown-item">
-                            Morgan Lyle
-                        </a>
-                        <a href="https://www.linkedin.com/"
-                            className="dropdown-item">
-                            Nellie Nelson
-                        </a>
-                        <a href="https://www.linkedin.com/"
-                            className="dropdown-item">
-                            Adrian Dorado
-                        </a>
-                        <a href="https://www.linkedin.com/"
-                            className="dropdown-item">
-                            Evan Wu
-                        </a>
-                    </li>
-                </ul>
-            </footer >
-        </div>
+        <MDBFooter className='text-center' color='#0D0C1D' >
+            <div className='text-center p-3' style={{ backgroundColor: '#aa98a9' }}>
+                <MDBContainer className='p-4'>
 
+                    <section className=''>
+                        <form action=''>
+                            <div className='row d-flex justify-content-center'>
+                                <div className='col-auto'>
+                                    <p className='pt-2'>
+                                        <strong>Sign up for a subscription box!</strong>
+                                    </p>
+                                </div>
 
+                                <MDBCol md='5' start='12'>
+                                    <MDBInput label='Email address' className='mb-4' />
+                                </MDBCol>
+
+                                <div className='col-auto'>
+                                    <MDBBtn outline color='dark' type='submit' className='mb-4'>
+                                        Subscribe!
+                                    </MDBBtn>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
+
+                    <section className=''>
+                        <p>
+                            Oh about us? Well for one we are super great.
+                        </p>
+                    </section>
+                    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#55acee' }}>
+                        <MDBIcon fab icon='twitter' size='lg' />
+                    </MDBBtn>
+                    <section className='mb-4'>
+                        <a className='btn btn-outline-dark btn-floating m-1' href='https://www.linkedin.com/in/morganlyle1/' role='button'>
+                            <MDBIcon fab icon='linkedin-in' />
+                        </a>
+
+                        <a className='btn btn-outline-dark btn-floating m-1' href='https://www.linkedin.com/in/morganlyle1/' role='button'>
+                            <MDBIcon fab icon='linkedin-in' />
+                        </a>
+
+                        <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                            <MDBIcon fab icon='linkedin-in' />
+                        </a>
+
+                        <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                            <MDBIcon fab icon='linkedin-in' />
+                        </a>
+
+                        <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                            <MDBIcon fab icon='github' />
+                        </a>
+                    </section>
+
+                    <section className=''>
+                        <MDBRow>
+                            <MDBDropdown>
+                                <MDBDropdownToggle tag='a' className='btn btn-light ob_button almond' >
+                                    Impulse!
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu>
+                                    <MDBDropdownItem>
+                                        <MDBDropdownLink href="#">Product Box!</MDBDropdownLink>
+                                    </MDBDropdownItem>
+                                    <MDBDropdownItem>
+                                        <MDBDropdownLink href="#"> Clothing Box!</MDBDropdownLink>
+                                    </MDBDropdownItem>
+                                    <MDBDropdownItem>
+                                        <MDBDropdownLink href="#">Quiz?</MDBDropdownLink>
+                                    </MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                            {/* <MDBCol lg='3' md='6' className='mb-4 mb-md-0'> */}
+                            {/* <h5 className='text-uppercase'>Impulse!</h5>
+
+                            <ul className='list-unstyled mb-0'>
+                                <li>
+                                    <a href='#!' className='text-dark'>
+                                        Product Box
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='#!' className='text-dark'>
+                                        Clothing Box
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='#!' className='text-dark'>
+                                        Different Subscription Boxes
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='#!' className='text-dark'>
+                                        Quiz?
+                                    </a>
+                                </li>
+                            </ul> */}
+                            {/* </MDBCol> */}
+
+                        </MDBRow>
+                    </section>
+                </MDBContainer>
+
+                © 2022 Copyright
+                <a className='text-dark' href='https://Impulse.com/'>
+                    ImpulseBox.com
+                </a>
+            </div>
+        </MDBFooter>
     );
 }
-
-export default Footer;
