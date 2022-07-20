323e415130490ef32c125f1dae441eae2a9d5a01
Morgan Lyle <morganlylee@gmail.com>
2022-07-20 17:58:14 -0400

    footer!!

--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,134 +1,79 @@
-// import React from 'react';
-// import {
-//     MDBFooter,
-//     MDBContainer,
-//     MDBIcon, MDBInput,
-//     MDBCol, MDBRow,
-//     MDBBtn, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem, MDBDropdownLink
-// } from 'mdb-react-ui-kit';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 // export default function Footer() {
 //     return (
-//         <MDBFooter className='text-center' color='#0D0C1D' >
-//             <div className='text-center p-3' style={{ backgroundColor: '#aa98a9' }}>
-//                 <MDBContainer className='p-4'>
-//                     <section className=''>
-//                         <form action=''>
-//                             <div className='row d-flex justify-content-center'>
-//                                 <div className='col-auto'>
-//                                     <p className='pt-2'>
-//                                         <strong>Sign up for a subscription box!</strong>
-//                                     </p>
-//                                 </div>
-//                                 <MDBCol md='5' start='12'>
-//                                     <MDBInput label='Email address' className='mb-4' />
-//                                 </MDBCol>
-//                                 <div className='col-auto'>
-//                                     <MDBBtn outline color='dark' type='submit' className='mb-4'>
-//                                         Subscribe!
-//                                     </MDBBtn>
-//                                 </div>
-//                             </div>
-//                         </form>
-//                     </section>
-//                     <section className=''>
-//                         <p>
-//                             Oh about us? Well for one we are super great.
-//                         </p>
-//                     </section>
-//                     <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#55acee' }}>
-//                         <MDBIcon fab icon='twitter' size='lg' />
-//                     </MDBBtn>
-//                     <section className='mb-4'>
-//                         <a className='btn btn-outline-dark btn-floating m-1' href='https://www.linkedin.com/in/morganlyle1/' role='button'>
-//                             <MDBIcon fab icon='linkedin-in' />
-//                         </a>
-//                         <a className='btn btn-outline-dark btn-floating m-1' href='https://www.linkedin.com/in/morganlyle1/' role='button'>
-//                             <MDBIcon fab icon='linkedin-in' />
-//                         </a>
-//                         <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
-//                             <MDBIcon fab icon='linkedin-in' />
-//                         </a>
-//                         <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
-//                             <MDBIcon fab icon='linkedin-in' />
-//                         </a>
-//                         <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
-//                             <MDBIcon fab icon='github' />
-//                         </a>
-//                     </section>
-//                     <section className=''>
-//                         <MDBRow>
-//                             <MDBDropdown>
-//                                 <MDBDropdownToggle tag='a' className='btn btn-light ob_button almond' >
-//                                     Impulse!
-//                                 </MDBDropdownToggle>
-//                                 <MDBDropdownMenu>
-//                                     <MDBDropdownItem>
-//                                         <MDBDropdownLink href="#">Product Box!</MDBDropdownLink>
-//                                     </MDBDropdownItem>
-//                                     <MDBDropdownItem>
-//                                         <MDBDropdownLink href="#"> Clothing Box!</MDBDropdownLink>
-//                                     </MDBDropdownItem>
-//                                     <MDBDropdownItem>
-//                                         <MDBDropdownLink href="#">Quiz?</MDBDropdownLink>
-//                                     </MDBDropdownItem>
-//                                 </MDBDropdownMenu>
-//                             </MDBDropdown>
-//                             {/* <MDBCol lg='3' md='6' className='mb-4 mb-md-0'> */}
-//                             {/* <h5 className='text-uppercase'>Impulse!</h5>
-//                             <ul className='list-unstyled mb-0'>
-//                                 <li>
-//                                     <a href='#!' className='text-dark'>
-//                                         Product Box
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <a href='#!' className='text-dark'>
-//                                         Clothing Box
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <a href='#!' className='text-dark'>
-//                                         Different Subscription Boxes
-//                                     </a>
-//                                 </li>
-//                                 <li>
-//                                     <a href='#!' className='text-dark'>
-//                                         Quiz?
-//                                     </a>
-//                                 </li>
-//                             </ul> */}
-//                             {/* </MDBCol> */}
-//                         </MDBRow>
-//                     </section>
-//                 </MDBContainer>
-//                 © 2022 Copyright
-//                 <a className='text-dark' href='https://Impulse.com/'>
-//                     ImpulseBox.com
-//                 </a>
+//         <>
+//             <div className="container">
+//                 {/* // <nav className="navbar navbar-expand-lg d-flex align-items-center fixed-bottom pb-2 mb-2 link-dark text-decoration-none bbcolor_bottom heliotrope_gray"> */}
+//                 <div className="navbar navbar-expand-lg d-flex align-items-center fixed-bottom pb-2 mb-2 link-dark text-decoration-none bbcolor_bottom heliotrope_gray container-fluid">
+//                     <NavLink className="navbar-brand text-left ms-2"
+//                         to="/">Impulse</NavLink>
+//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span className="navbar-toggler-icon"></span>
+//                     </button>
+//                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//                         <ul className="navbar-nav">
+
+//                             <li className="nav-item">
+//                                 <NavLink className="nav-link" to="/aboutus">Oh, about us? Well we are pretty great for one.</NavLink>
+//                             </li>
+//                         </ul>
+//                         <ul className="navbar-nav text-right">
+//                             <li className="nav-item dropup ms-auto text-right">
+//                                 <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                                     Links
+//                                 </NavLink>
+//                                 <ul className="dropdown-menu text-right">
+//                                     <li><NavLink className="dropdown-item" to="#">Morgan LinkedIn</NavLink></li>
+//                                     <li><NavLink className="dropdown-item" to="#">Evan LinkedIn</NavLink></li>
+//                                     <li><NavLink className="dropdown-item" to="#">Adrian LinkedIn</NavLink></li>
+//                                     <li><NavLink className="dropdown-item" to="#">Nellie LinkedIn</NavLink></li>
+//                                 </ul>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 {/* // </nav> */}
 //             </div>
-//         </MDBFooter>
+//         </>
 //     );
 // }
-
-import { NavLink } from 'react-router-dom';
-
-export default function NavBar() {
+export default function Footer() {
     return (
         <>
-            <div>
-                <div href="/" className="d-flex align-items-center pb-2 mb-2 link-dark text-decoration-none bbcolor_top heliotrope_gray">
+            <div className="container">
+                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom pb-2 mb-2 link-dark text-decoration-none bbcolor_bottom heliotrope_gray ">
+                    <span className="ms-3 fs-2 fw-semibold nameText"></span>
+                    <span className="col d-flex align-items-center text-center justify-content-center link-dark px-2 text-muted">© 2022 Copyright Impulse Box</span>
+                    <span className="ms-2 me-3 fs-1 fw-semibold nameText almond">||</span>
+                    <NavLink to="/aboutus" className="col-md-4 d-flex align-items-center text-center justify-content-center link-dark nav-link px-2 text-muted">
+                        {/* <svg className="bi me-2" width="40" height="32"><use to="/aboutus"> */}
+                        Oh, about us? Well we are pretty great for one.
+                    </NavLink>
                     <span className="ms-3 fs-2 fw-semibold nameText"></span>
                     <span className="ms-2 fs-1 fw-semibold nameText almond">||</span>
-                    <span className="ms-1 mt-2 fs-4 fw-semibold nameText"></span>
-                    <span className=" ms-auto text-center">© 2022 Copyright Impulse Box</span>
-                    <NavLink className="text-right link-dark text-decoration-none" to="/aboutus">
-                        <button className="text-right btn btn-toggle rounded border-0 collapsed fs-5 fw-semibold siteText">
-                            Oh, about us? Well, for one, we're pretty great.
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
+
+                    <ul className="nav col-md-4 mb-0 justify-content-center">
+                        <span className="navbar-nav">
+                            <li className="nav-item dropup ms-auto text-muted">
+                                <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" text-muted>
+                                    Links
+                                </NavLink>
+                            </li>
+                            <span className="dropdown-menu">
+                                <li><NavLink classNameName="dropdown-item" to="#">Morgan LinkedIn</NavLink></li>
+                                <li><NavLink classNameName="dropdown-item" to="#">Evan LinkedIn</NavLink></li>
+                                <li><NavLink classNameName="dropdown-item" to="#">Adrian LinkedIn</NavLink></li>
+                                <li><NavLink classNameName="dropdown-item" to="#">Nellie LinkedIn</NavLink></li>
+                            </span>
+                        </span>
+                        <li ><NavLink to="#" className="nav-link px-2 text-muted">Home</NavLink></li>
+                        <li><NavLink to="#" className="nav-link px-2 text-muted">Features</NavLink></li>
+                        <li><NavLink to="#" className="nav-link px-2 text-muted">Pricing</NavLink></li>
+                    </ul>
+                </footer>
+            </div >
         </>
     )
 }
