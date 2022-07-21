0ff6d9bbbdd91e5998f8d25a048577453abc7b8f
Morgan Lyle <morganlylee@gmail.com>
2022-07-21 15:50:49 -0400

    footer to main

--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,44 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-// export default function Footer() {
-//     return (
-//         <>
-//             <div className="container">
-//                 {/* // <nav className="navbar navbar-expand-lg d-flex align-items-center fixed-bottom pb-2 mb-2 link-dark text-decoration-none bbcolor_bottom heliotrope_gray"> */}
-//                 <div className="navbar navbar-expand-lg d-flex align-items-center fixed-bottom pb-2 mb-2 link-dark text-decoration-none bbcolor_bottom heliotrope_gray container-fluid">
-//                     <NavLink className="navbar-brand text-left ms-2"
-//                         to="/">Impulse</NavLink>
-//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-//                         <span className="navbar-toggler-icon"></span>
-//                     </button>
-//                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//                         <ul className="navbar-nav">
-
-//                             <li className="nav-item">
-//                                 <NavLink className="nav-link" to="/aboutus">Oh, about us? Well we are pretty great for one.</NavLink>
-//                             </li>
-//                         </ul>
-//                         <ul className="navbar-nav text-right">
-//                             <li className="nav-item dropup ms-auto text-right">
-//                                 <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                                     Links
-//                                 </NavLink>
-//                                 <ul className="dropdown-menu text-right">
-//                                     <li><NavLink className="dropdown-item" to="#">Morgan LinkedIn</NavLink></li>
-//                                     <li><NavLink className="dropdown-item" to="#">Evan LinkedIn</NavLink></li>
-//                                     <li><NavLink className="dropdown-item" to="#">Adrian LinkedIn</NavLink></li>
-//                                     <li><NavLink className="dropdown-item" to="#">Nellie LinkedIn</NavLink></li>
-//                                 </ul>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//                 {/* // </nav> */}
-//             </div>
-//         </>
-//     );
-// }
 export default function Footer() {
     return (
         <>
