19cabbbf332d4a2cd880432f2c4ec6966847f77f
Evan <evanx150@gmail.com>
2022-07-25 18:34:48 -0500

    backend auth

--- subscriptions/api/subscriptions/urls.py
+++ subscriptions/api/subscriptions/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('subscriptions_rest.urls'))
+    path('api/', include('subscriptions_rest.urls')),
+    path("", include("djwto.urls")),
 ]
