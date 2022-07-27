6f2419fc455d01ecb38724dc7ccdec6f0b636a38
Morgan Lyle <morganlylee@gmail.com>
2022-07-27 00:50:59 -0400

    merge

--- products/products/settings.py
+++ products/products/settings.py
@@ -14,8 +14,6 @@ from datetime import timedelta
 from pathlib import Path
 import dj_database_url
 
-from subscriptions.api.subscriptions.settings import DJWTO_CSRF  # type: ignore
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
