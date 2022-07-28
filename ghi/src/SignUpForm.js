0c320df6e4939f8eb42d041de4b4573f7d74dbd4
rbbitphoto <rbbitphoto@gmail.com>
2022-07-28 16:34:30 -0700

    need to pull from main

--- ghi/src/SignUpForm.js
+++ ghi/src/SignUpForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class SignUpForm extends React.Component {
+export default class SignUpForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,22 +17,10 @@ class SignUpForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-
-  // handleChange(event) {
-  //   const newState = {};
-  //   if (event.target.password === event.target.password_verify) {
-  //     newState[event.target.id] = event.target.value;
-  //     this.setState(newState);
-  //   } else {
-  //     // they must go back and fix so that passwords match
-  //   }
-  // }
-
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value })
   }
 
-  // do I need a componentDidMount function? Not yet sure what that component would be here.
 
   async handleSubmit(event) {
     if (this.state['password'] === this.state['password_verify']) {
@@ -48,7 +36,7 @@ class SignUpForm extends React.Component {
           'Content-Type': 'application/json',
         },
       };
-  
+
       const response = await fetch(signUpUrl, fetchConfig);
       if (response.ok) {
         const newSignUp = await response.json();
@@ -70,7 +58,7 @@ class SignUpForm extends React.Component {
 
   render() {
     return (
-      <div style={{ paddingBottom: '150px'}} className="row">
+      <div style={{ paddingBottom: '150px' }} className="row">
         <div className="offset-3 col-6">
           <div className="b_cards shad_intense p-4 rounded mt-4 heliotrope_gray">
             <h1 className="nameText pb-3">Sign up for your subscription box!</h1>
@@ -112,4 +100,48 @@ class SignUpForm extends React.Component {
   };
 };
 
-export default SignUpForm;
+
+// export default function SignUpForm() {
+//   return (
+//     <>
+//       <div style={{ paddingBottom: '150px' }} className="row">
+//         <div className="offset-3 col-6">
+//           <div className="b_cards shad_intense p-4 rounded mt-4 heliotrope_gray">
+//             <h1 className="nameText pb-3">Sign up for your subscription box!</h1>
+//             <form onSubmit={this.handleSubmit} id="create-signup-form">
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.username} placeholder="Username" type="text" id="username" className="siteText form-control" />
+//                 <label htmlFor="name">Username</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.email} placeholder="Email address" type="text" id="email" className="siteText form-control" />
+//                 <label htmlFor="name">Email address</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.password} placeholder="Password" type="password" id="password" className="siteText form-control" />
+//                 <label htmlFor="name">Password</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.password_verify} placeholder="Enter password again" type="password" id="password_verify" className="siteText form-control" />
+//                 <label htmlFor="name">Enter password again</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.first_name} placeholder="First Name" type="text" id="first_name" className="siteText form-control" />
+//                 <label htmlFor="first_name">First Name</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.last_name} placeholder="Last Name" type="text" id="last_name" className="siteText form-control" />
+//                 <label htmlFor="first_name">Last Name</label>
+//               </div>
+//               <div className="inputText form-floating mb-3">
+//                 <input onChange={this.handleChange} value={this.state.address} placeholder="Address" type="text" id="address" className="siteText form-control" />
+//                 <label htmlFor="first_name">Address</label>
+//               </div>
+//               <button className="btn btn-light siteText almond ob_button">Sign up</button>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
