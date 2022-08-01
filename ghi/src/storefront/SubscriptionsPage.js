4f2fcf064d6dfd226927494acf13bd6098a856f7
Morgan Lyle <morganlylee@gmail.com>
2022-08-01 18:16:44 -0400

    button links

--- ghi/src/storefront/SubscriptionsPage.js
+++ ghi/src/storefront/SubscriptionsPage.js
@@ -6,11 +6,11 @@ export default function SubscriptionsPage() {
                 <div className='row'>
                     <div className='col b_cards me-3 shad_intense p-4 rounded mt-4 heliotrope_gray'>
                         <h1 className='display-3 siteText text-left'>Things Box</h1>
-                        <button className='btn btn-light almond ob_button text-right'>Add to Cart!</button>
+                        <a href="/checkout" className='btn btn-light almond ob_button text-right'>Add to Cart!</a>
                     </div>
                     <div className='col b_cards ms-3 shad_intense p-4 rounded mt-4 heliotrope_gray'>
                         <h1 className='display-3 siteText text-right'>Styles Box</h1>
-                        <button className='btn btn-light almond ob_button text-left'>Add to Cart!</button>
+                        <a href="/checkout" className='btn btn-light almond ob_button text-left'>Add to Cart!</a>
                     </div>
                 </div>
             </div>
