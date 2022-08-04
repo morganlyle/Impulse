b8c4cd8c424ed55f1c0def36f767ba0c1b610a11
Morgan Lyle <morganlylee@gmail.com>
2022-08-04 14:37:36 -0400

    Unit test

--- subscriptions/api/subscriptions_rest/test/test_2.py
+++ subscriptions/api/subscriptions_rest/test/test_2.py
@@ -3,24 +3,6 @@ from django.urls import reverse
 from subscriptions_rest.models import Receipt, User
 
 class TestViews(TestCase):
-    # def setUp(self):
-    #     User.objects.create(
-    #         username="impulse",
-    #         email="test@test.com",
-    #         first_name="Test",
-    #         last_name="Testy",
-    #         password="password1234",
-    #     )
-
-        # *working*
-    # def test_show_receipt(self):
-    #     client = Client()
-    #     response = client.get(reverse("receipt_list"))
-    #     self.assertEquals(response.status_code, 200)
-    #     # *working*
-    # def test_pages_load_properly(self):
-    #     response = self.client.get('http://localhost:3000/')
-    #     self.assertEqual(response.status_code, 404)
         
     def test_login_works(self):
         User.objects.create_user("nerd", password="password1234")
