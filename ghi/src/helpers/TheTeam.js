2603575733d775f02b49cbe605d625f4fecb4af4
Morgan Lyle <morganlylee@gmail.com>
2022-08-04 19:27:50 -0400

    about me

--- ghi/src/helpers/TheTeam.js
+++ ghi/src/helpers/TheTeam.js
@@ -11,6 +11,10 @@ export default function TheTeam() {
                             <div className="card-body text-center">
                                 <h5 className="card-title mb-0">Morgan Lyle</h5>
                                 <div className="card-text text-black-50">Full Stack Developer</div>
+                                <hr className="featurette-divider" />
+                                <p className="card-text text-left">
+                                    About me!
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -20,6 +24,7 @@ export default function TheTeam() {
                             <div className="card-body text-center">
                                 <h5 className="card-title mb-0">Team Member</h5>
                                 <div className="card-text text-black-50">Web Developer</div>
+
                             </div>
                         </div>
                     </div>
