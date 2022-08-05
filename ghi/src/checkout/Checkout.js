3c760babe82886d4eb0056c586f62ac9a746694d
Morgan Lyle <morganlylee@gmail.com>
2022-08-05 12:30:26 -0400

    pre merge

--- ghi/src/checkout/Checkout.js
+++ ghi/src/checkout/Checkout.js
@@ -32,7 +32,7 @@ export default function Checkout() {
             orderNumber,
         }
         console.log(data);
-        const receiptUrl = `${process.env.REACT_APP}/api/receipts/`
+        const receiptUrl = `${process.env.REACT_APP_SUBSCRIPTIONS}/api/receipts/`
         const fetchConfig = {
             method: "post",
             body: JSON.stringify(data),
@@ -92,79 +92,79 @@ export default function Checkout() {
                         </Card>
                         <h4 className="mb-3 siteText">Billing address</h4>
                         <form onSubmit={handleSubmit} id='create-Receipt-form'>
-                        <Card className='bgcolor p-4 b_cards shad_light'>
-                            <div className="row g-3 nameText">
-                                <div className="col-sm-6">
-                                    <label className="form-label">First name</label>
-                                    <input onChange={e => setFirstName(e.target.value)} value={firstName} type="text" className="form-control" id="firstName" placeholder="" />
-                                    <div className="invalid-feedback">
-                                        Valid first name is required.
-                                    </div>
-                                </div>
-                                <div className="col-sm-6">
-                                    <label className="form-label">Last name</label>
-                                    <input onChange={e => setLastName(e.target.value)} value={lastName} type="text" className="form-control" id="lastName" placeholder="" required="" />
-                                    <div className="invalid-feedback">
-                                        Valid last name is required.
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <label className="form-label">Username</label>
-                                    <div className="input-group has-validation">
-                                        <span className="input-group-text">@</span>
-                                        <input onChange={e => setUsername(e.target.value)} value={username} type="text" className="form-control" id="username" placeholder="Username" required="" />
+                            <Card className='bgcolor p-4 b_cards shad_light'>
+                                <div className="row g-3 nameText">
+                                    <div className="col-sm-6">
+                                        <label className="form-label">First name</label>
+                                        <input onChange={e => setFirstName(e.target.value)} value={firstName} type="text" className="form-control" id="firstName" placeholder="" />
                                         <div className="invalid-feedback">
-                                            Your username is required.
+                                            Valid first name is required.
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <label className="form-label">Last name</label>
+                                        <input onChange={e => setLastName(e.target.value)} value={lastName} type="text" className="form-control" id="lastName" placeholder="" required="" />
+                                        <div className="invalid-feedback">
+                                            Valid last name is required.
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <label className="form-label">Username</label>
+                                        <div className="input-group has-validation">
+                                            <span className="input-group-text">@</span>
+                                            <input onChange={e => setUsername(e.target.value)} value={username} type="text" className="form-control" id="username" placeholder="Username" required="" />
+                                            <div className="invalid-feedback">
+                                                Your username is required.
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <label className="form-label">Email <span className="text-muted">(Optional)</span></label>
+                                        <input onChange={e => setEmail(e.target.value)} value={email} type="email" className="form-control" id="email" placeholder="you@example.com" />
+                                        <div className="invalid-feedback">
+                                            Please enter a valid email address for shipping updates.
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <label className="form-label">Address</label>
+                                        <input onChange={e => setAddress(e.target.value)} value={address} type="text" className="form-control" id="address" placeholder="1234 Main St" required="" />
+                                        <div className="invalid-feedback">
+                                            Please enter your shipping address.
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        <label className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
+                                        <input onChange={e => setAddress2(e.target.value)} value={address2} type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
+                                    </div>
+                                    <div className="col-md-5">
+                                        <label className="form-label">Country</label>
+                                        <select onChange={e => setCountry(e.target.value)} value={country} className="form-select" id="country" required="">
+                                            <option value="">Choose...</option>
+                                            <option>United States</option>
+                                        </select>
+                                        <div className="invalid-feedback">
+                                            Please select a valid country.
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <label className="form-label">State</label>
+                                        <select onChange={e => setState(e.target.value)} value={state} className="form-select" id="state" required="">
+                                            <option value="">Choose...</option>
+                                            <option>California</option>
+                                        </select>
+                                        <div className="invalid-feedback">
+                                            Please provide a valid state.
+                                        </div>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <label className="form-label">Zip</label>
+                                        <input onChange={e => setZip(e.target.value)} value={zip} type="text" className="form-control" id="zip" placeholder="" required="" />
+                                        <div className="invalid-feedback">
+                                            Zip code required.
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-12">
-                                    <label className="form-label">Email <span className="text-muted">(Optional)</span></label>
-                                    <input onChange={e => setEmail(e.target.value)} value={email} type="email" className="form-control" id="email" placeholder="you@example.com" />
-                                    <div className="invalid-feedback">
-                                        Please enter a valid email address for shipping updates.
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <label className="form-label">Address</label>
-                                    <input onChange={e => setAddress(e.target.value)} value={address} type="text" className="form-control" id="address" placeholder="1234 Main St" required="" />
-                                    <div className="invalid-feedback">
-                                        Please enter your shipping address.
-                                    </div>
-                                </div>
-                                <div className="col-12">
-                                    <label className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
-                                    <input onChange={e => setAddress2(e.target.value)} value={address2} type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
-                                </div>
-                                <div className="col-md-5">
-                                    <label className="form-label">Country</label>
-                                    <select onChange={e => setCountry(e.target.value)} value={country} className="form-select" id="country" required="">
-                                        <option value="">Choose...</option>
-                                        <option>United States</option>
-                                    </select>
-                                    <div className="invalid-feedback">
-                                        Please select a valid country.
-                                    </div>
-                                </div>
-                                <div className="col-md-4">
-                                    <label className="form-label">State</label>
-                                    <select onChange={e => setState(e.target.value)} value={state} className="form-select" id="state" required="">
-                                        <option value="">Choose...</option>
-                                        <option>California</option>
-                                    </select>
-                                    <div className="invalid-feedback">
-                                        Please provide a valid state.
-                                    </div>
-                                </div>
-                                <div className="col-md-3">
-                                    <label className="form-label">Zip</label>
-                                    <input onChange={e => setZip(e.target.value)} value={zip} type="text" className="form-control" id="zip" placeholder="" required="" />
-                                    <div className="invalid-feedback">
-                                        Zip code required.
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
+                            </Card>
                         </form>
                     </div>
                 </div>
