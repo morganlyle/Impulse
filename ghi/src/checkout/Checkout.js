979173fd0f92e1a00ef13c8dfd54651652166cfe
rbbitphoto <rbbitphoto@gmail.com>
2022-08-05 09:27:58 -0700

    added inventory.json file, modified links with env variables

--- ghi/src/checkout/Checkout.js
+++ ghi/src/checkout/Checkout.js
@@ -32,7 +32,7 @@ export default function Checkout() {
             orderNumber,
         }
         console.log(data);
-        const receiptUrl = `${process.env.REACT_APP}/api/receipts/`
+        const receiptUrl = `${process.env.REACT_APP_SUBSCRIPTIONS}/api/receipts/`
         const fetchConfig = {
             method: "post",
             body: JSON.stringify(data),
